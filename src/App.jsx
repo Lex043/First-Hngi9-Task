486449ea5b1c8d44d72fa38573eced43e57d67bb
Oyebamiji Alex <alexferanmi390@gmail.com>
2022-10-22 12:09:48 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Linktree from "./components/Linktree";
+
+const App = () => {
+  return (
+    <main className="my-10 mx-5">
+      <Linktree />
+    </main>
+  );
+};
+
+export default App;
---
+++ src/components/Linktree.jsx
@@ -0,0 +1,71 @@
+import profile from "../assets/p16787843_i_h9_ab.jpg";
+
+const Linktree = () => {
+  return (
+    <section className="bg-white w-full">
+      <div className="flex justify-center flex-col gap-5">
+        {/*profile__img  */}
+        <img
+          src={profile}
+          alt=""
+          className="rounded-full w-20 h-20 object-cover mx-auto"
+        />
+
+        {/* twitterid */}
+        <p className="text-center mb-3">Lex_Feranmi</p>
+
+        {/* twitter link */}
+        <button className="bg-[#eaecf0] py-5 rounded-md">
+          <a href="https://twitter.com/Lex_Feranmi" target="_blank">
+            Twitter Link
+          </a>
+        </button>
+
+        {/* slack id */}
+        <button className="hidden bg-[#eaecf0] py-5 rounded-md">
+          <p>Lex043</p>
+        </button>
+
+        {/* btn__zuri */}
+        <button className="bg-[#eaecf0] py-5 rounded-md">
+          <a href="https://training.zuri.team/" target="_blank">
+            Zuri Team
+          </a>
+        </button>
+
+        {/* books */}
+        <button className="bg-[#eaecf0] py-5 rounded-md">
+          <a href="http://books.zuri.team" target="_blank">
+            Zuri Books
+          </a>
+        </button>
+
+        {/* book__python */}
+        <button className="bg-[#eaecf0] py-5 rounded-md">
+          <a
+            href="https://books.zuri.team/python-for-beginners?ref_id=Lex043"
+            target="_blank"
+          >
+            Python Books
+          </a>
+        </button>
+
+        {/* pitch */}
+        <button className="bg-[#eaecf0] py-5 rounded-md">
+          <a href="https://background.zuri.team" target="_blank">
+            Background Checkers for Coders
+          </a>
+        </button>
+
+        {/* book__design */}
+        <button className="bg-[#eaecf0] py-5 rounded-md">
+          <a href="https://books.zuri.team/design-rules" target="_blank">
+            Design Books
+          </a>
+        </button>
+      </div>
+    </section>
+  );
+};
+
+export default Linktree;
