abbeefd09831d1f1fda94d5b123906faa24c9167
Oyebamiji Alex <alexferanmi390@gmail.com>
2022-10-28 01:50:20 +0100

    feat: add id's

--- src/components/Linktree.jsx
+++ src/components/Linktree.jsx
@@ -4,63 +4,72 @@ const Linktree = () => {
   return (
     <section className="bg-white w-full">
       <div className="flex justify-center flex-col gap-5">
-        {/*profile__img  */}
         <img
           src={profile}
           alt=""
           className="rounded-full w-20 h-20 object-cover mx-auto"
+          id="profile__img"
         />
+        <p className="text-center mb-3" id="twitter">
+          Lex_Feranmi
+        </p>
+        <p className="hidden" id="slack">
+          Lex043
+        </p>
 
-        {/* twitterid */}
-        <p className="text-center mb-3">Lex_Feranmi</p>
+        <a
+          className="bg-[#eaecf0] py-5 rounded-md text-center"
+          id="twitter"
+          href="https://twitter.com/Lex_Feranmi"
+          target="_blank"
+        >
+          Twitter Link
+        </a>
 
-        {/* slack id */}
-        <p className="hidden">Lex043</p>
+        <a
+          className="bg-[#eaecf0] py-5 rounded-md text-center"
+          id="btn__zuri"
+          href="https://training.zuri.team/"
+          target="_blank"
+        >
+          Zuri Team
+        </a>
 
-        {/* twitter link */}
-        <button className="bg-[#eaecf0] py-5 rounded-md">
-          <a href="https://twitter.com/Lex_Feranmi" target="_blank">
-            Twitter Link
-          </a>
-        </button>
+        <a
+          className="bg-[#eaecf0] py-5 rounded-md text-center"
+          id="books"
+          href="http://books.zuri.team"
+          target="_blank"
+        >
+          Zuri Books
+        </a>
 
-        {/* btn__zuri */}
-        <button className="bg-[#eaecf0] py-5 rounded-md">
-          <a href="https://training.zuri.team/" target="_blank">
-            Zuri Team
-          </a>
-        </button>
+        <a
+          className="bg-[#eaecf0] py-5 rounded-md text-center"
+          id="book__python"
+          href="https://books.zuri.team/python-for-beginners?ref_id=Lex043"
+          target="_blank"
+        >
+          Python Books
+        </a>
 
-        {/* books */}
-        <button className="bg-[#eaecf0] py-5 rounded-md">
-          <a href="http://books.zuri.team" target="_blank">
-            Zuri Books
-          </a>
-        </button>
+        <a
+          className="bg-[#eaecf0] py-5 rounded-md text-center"
+          id="pitch"
+          href="https://background.zuri.team"
+          target="_blank"
+        >
+          Background Check for Coders
+        </a>
 
-        {/* book__python */}
-        <button className="bg-[#eaecf0] py-5 rounded-md">
-          <a
-            href="https://books.zuri.team/python-for-beginners?ref_id=Lex043"
-            target="_blank"
-          >
-            Python Books
-          </a>
-        </button>
-
-        {/* pitch */}
-        <button className="bg-[#eaecf0] py-5 rounded-md">
-          <a href="https://background.zuri.team" target="_blank">
-            Background Checkers for Coders
-          </a>
-        </button>
-
-        {/* book__design */}
-        <button className="bg-[#eaecf0] py-5 rounded-md">
-          <a href="https://books.zuri.team/design-rules" target="_blank">
-            Design Books
-          </a>
-        </button>
+        <a
+          className="bg-[#eaecf0] py-5 rounded-md text-center"
+          id="book__design"
+          href="https://books.zuri.team/design-rules"
+          target="_blank"
+        >
+          Design Books
+        </a>
       </div>
     </section>
   );
